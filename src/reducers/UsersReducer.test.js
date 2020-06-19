057d26c838ff15414ffc086251cd8007b1d65c4e
hcpioquinto <harveycharles.pioquinto@devpartners.co>
2020-06-20 00:09:47 +0800

    Additional testing

--- src/reducers/UsersReducer.test.js
+++ src/reducers/UsersReducer.test.js
@@ -1,11 +1,10 @@
 import usersReducer from './UsersReducer';
 import { GET_USERS, SET_USER } from '../actions/UsersActions';
 
-
-
 describe('Users Reducer ', () => {
   const initState = { 
     userList: [],
+    selectedUser: 0,
   };
   
   test('should store new users on GET_USERS', () => {
@@ -14,8 +13,13 @@ describe('Users Reducer ', () => {
       {id: 2, name: 'Metrow'},
     ];
     const res = usersReducer(initState, {type: GET_USERS, payload: userList});
-    expect(res).toEqual({ userList });
+    expect(res).toEqual({ ...initState, userList });
   });
-  
 
+  test('should store selected user on SET_USERS', () => {
+    const selectedUser = 69;
+    const res = usersReducer(initState, {type: SET_USER, payload: selectedUser
+    });
+    expect(res).toEqual({ ...initState, selectedUser });
+  });
 });
